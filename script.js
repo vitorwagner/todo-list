a67f919d569b660c03fcfe87360d0f9f549322aa
Vitor Wagner <vitormwagner@gmail.com>
2022-09-02 15:09:59 -0300

    Adiciona condicional para impedir entradas vazias na lista e limpa input após inserir item

--- script.js
+++ script.js
@@ -3,9 +3,11 @@ const list = document.getElementById('lista-tarefas');
 const inputTask = document.getElementById('texto-tarefa');
 
 function createTask() {
+  if (inputTask.value === '') return;
   const task = document.createElement('li');
   task.innerText = inputTask.value;
   list.appendChild(task);
+  inputTask.value = '';
 }
 
 createTaskButton.addEventListener('click', createTask);

feff60ab5226cdb7ff4e00076cb7ab9fd266f28b
Vitor Wagner <vitormwagner@gmail.com>
2022-09-10 23:04:39 -0300

    Adiciona botão para remover tarefas concluídas

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const list = document.getElementById('lista-tarefas');
 const inputTask = document.getElementById('texto-tarefa');
 const clearButton = document.getElementById('apaga-tudo');
 const saveButton = document.getElementById('salvar-tarefas');
+const clearDoneButton = document.getElementById('remover-finalizados');
 
 function createTask() {
   if (inputTask.value === '') return;
@@ -37,4 +38,11 @@ clearButton.addEventListener('click', () => {
 });
 saveButton.addEventListener('click', () => {
   localStorage.setItem('List', list.innerHTML);
+});
+clearDoneButton.addEventListener('click', () => {
+  for (let i = list.children.length - 1; i >= 0; i -= 1) {
+    if (list.children[i].classList.contains('completed')) {
+      list.children[i].remove();
+    }
+  }
 });
